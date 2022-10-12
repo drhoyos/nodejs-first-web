57ab89303249eb55dd8089fb67604933315f0d50
drhoyos <hoyos.dario@gmail.com>
2022-10-12 20:09:37 -0300

    first

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static( join(__dirname, 'public')))
+
+app.get("/", (req, res) => {
+  res.render("index",{title: 'web index'});
+});
+
+app.get("/test", (req, res) => {
+    res.render("test",{title: 'web test'});
+  });
+
+app.listen(3000);
+console.log("Servidor en PORT:", 3000);
